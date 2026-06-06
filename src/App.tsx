1558ef6391a157cad94551988a7d52a143b922d7
jeffkaberagithara-guru <jeffkaberagithara@gmail.com>
2026-06-06 20:11:48 +0300

    Modified app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./context/ThemeContext";
+import { SearchProvider } from "./context/SearchContext";
 import { BudgetProvider } from "./context/BudgetContext";
 import Layout from "./components/Layout";
 import Dashboard from "./pages/Dashboard";
 import Transactions from "./pages/Transactions";
 import Reports from "./pages/Reports";
 import Savings from "./pages/Savings";
+import Settings from "./pages/Settings";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <BudgetProvider>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/transactions" element={<Transactions />} />
-            <Route path="/reports" element={<Reports />} />
-            <Route path="/savings" element={<Savings />} />
-          </Routes>
-        </Layout>
-      </BudgetProvider>
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <BudgetProvider>
+          <SearchProvider>
+            <Layout>
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/transactions" element={<Transactions />} />
+                <Route path="/reports" element={<Reports />} />
+                <Route path="/savings" element={<Savings />} />
+                <Route path="/settings" element={<Settings />} />
+              </Routes>
+            </Layout>
+          </SearchProvider>
+        </BudgetProvider>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
